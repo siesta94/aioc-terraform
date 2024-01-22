960bd637445ef34f47f3551b5f142d00e00a1e77
Strahinja Tasic <strahinja.tasic@playstudios-il.com>
2024-01-22 10:51:28 +0100

    Adding default variables for network

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -20,14 +20,17 @@ variable "cidr" {
 
 variable "private_subnets" {
   type = list(string)
+  default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
 }
 
 variable "public_subnets" {
   type = list(string)
+  default = [ "10.0.4./24", "10.0.5.0/24", "10.0.6.0/24" ]
 }
 
 variable "azs" {
   type = list(string)
+  default = [ "us-east-1a", "us-east-1b", "us-east-1c" ]
 }
 
 variable "enable_nat_gateway" {
