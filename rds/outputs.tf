d1a69b0338c91a903913fd14c858aaaa859daecf
Strahinja Tasic <strahinja.tasic@playstudios-il.com>
2024-01-22 15:32:33 +0100

    Adding output for SG

--- rds/outputs.tf
+++ rds/outputs.tf
@@ -2,3 +2,8 @@ output "db_endpoint" {
   value       = module.db.db_instance_endpoint
   description = "End point of RDS instance"
 }
+
+output "rds_sg" {
+  value = module.rds_sg.security_group_id
+  description = "Security Group ID for RDS"
+}
