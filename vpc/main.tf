c07ba526917d162a04f3421e54522d2243809c9b
Strahinja Tasic <strahinja.tasic@playstudios-il.com>
2024-01-22 11:25:04 +0100

    Adding DB subnet support

--- vpc/main.tf
+++ vpc/main.tf
@@ -3,14 +3,20 @@ module "vpc" {
 
   name = var.vpc_name
   cidr = var.cidr
-
+# Networking stuff for HA / or single AZ's, check variable values, defaults are healthy
   azs             = var.azs
   private_subnets = var.private_subnets
   public_subnets  = var.public_subnets
 
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
   enable_nat_gateway = var.enable_nat_gateway
+  single_nat_gateway = var.single_nat_gateway
   enable_vpn_gateway = var.enable_vpn_gateway
   reuse_nat_ips = var.reuse_nat_ips
+
+# DB subnets if neccessery
+  create_database_subnet_group	= var.create_database_subnet_group
 
   tags = {
     Terraform = "true"
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -10,12 +10,17 @@ variable "terraform_cloud_secret_key" {
   type = string
 }
 
+variable "environment" {
+  type = string
+}
+
 variable "vpc_name" {
   type = string
 }
 
 variable "cidr" {
   type = string
+  default = "10.0.0.0/16"
 }
 
 variable "private_subnets" {
@@ -33,18 +38,45 @@ variable "azs" {
   default = [ "us-east-1a", "us-east-1b", "us-east-1c" ]
 }
 
+variable "single_nat_gateway" {
+  type = string
+  default = "true"
+}
 variable "enable_nat_gateway" {
   type = string
+  default = "true"
 }
 
 variable "enable_vpn_gateway" {
   type = string
+  default = "false"
 }
 
+variable "enable_dns_hostnames" {
+  type = string
+  default = "true"
+}
+
+variable "enable_dns_support" {
+  type = string
+  default = "true"
+}
 variable "reuse_nat_ips" {
   type = string
+  default = "true"
 }
 
-variable "environment" {
-  type = string
+variable "create_database_subnet_group" {
+  type = bool
+  default = false
+}
+
+variable "database_subnets_cidr_blocks" {
+  type = list(string)
+  default = ["10.0.21.0/24", "10.0.22.0/24"]
+}
+
+variable "create_database_subnet_route_table" {
+  type = bool
+  default = false
 }
